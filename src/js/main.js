e6d359a1d5a746d0d9129e20ceebfba4a76206f2
jcaldia900 <j.calderon.diaz@iescristobaldemonroy.es>
2021-04-16 14:08:15 +0200

    confirmacion final 16.04

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
-console.log('hjsdhsaj')
+import Swiper from 'swiper';
+var swiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+
+
+function Jesus() {
+    console.log('hola jesus')
+
+}
+
+
+
+let div = document.getElementById('prpr')
+
+div.onclick = Jesus
